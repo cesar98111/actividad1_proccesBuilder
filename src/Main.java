57a8833a8ba176b460d2771f8d969c2b21c9cb17
cesar jesus leon martin <cesilius98@gmail.com>
2022-10-18 17:24:12 +0000

    Feature: implementacion de apertura de pantallas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command("firefox");
+        Process proceso [] = new Process[5];
 
+        try{
+            for (int i = 0; i < 5; i++) {
+                proceso[i] = pb.start();
+            }
+
+            for (int i = 0; i < 5; i++) {
+                proceso[i].waitFor();
+            }
+        }catch (IOException | InterruptedException e){
+            System.out.println("no funciona");
+        }
 
     }
 }
