8d10bb40fc872dac7c187aa99965af8162136ef9
cesar jesus leon martin <cesilius98@gmail.com>
2022-10-18 17:33:01 +0000

    Feature: aplicada interacción con usuario

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ProcessBuilder pb = new ProcessBuilder();
         pb.command("firefox");
-        Process proceso [] = new Process[5];
+
+        Process proceso [] = new Process[sc.nextInt()];
 
         try{
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < proceso.length; i++) {
                 proceso[i] = pb.start();
             }
 
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < proceso.length; i++) {
                 proceso[i].waitFor();
             }
         }catch (IOException | InterruptedException e){
